3d55e587cc6db6438032397da09cd40588b0e595
nk19963 <kn_1996_0303@yahoo.co.jp>
2024-02-25 11:43:22 +0900

    install chakura ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 /** @format */
 
+import { Box, Center, CheckboxGroup, Text } from "@chakra-ui/react";
 import Task from "./component/Task";
 
 const App = () => {
   return (
-    <>
-      <h1>タスク一覧</h1>
-      <Task name="買い物" />
-      <Task name="ランニング" />
-      <Task name="プログラミングの勉強" />
-    </>
+    <Box mt="64px">
+      <Center>
+        <Box>
+          <Box mb="24px">
+            <Text fontSize="24px" fontWeight="bold">
+              <h1>タスク一覧</h1>
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            <Task name="買い物" />
+            <Task name="ランニング" />
+            <Task name="プログラミングの勉強" />
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
   );
 };
 
--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,7 +1,15 @@
+/** @format */
+
+import { Box, Checkbox, Text } from "@chakra-ui/react";
+
 const Task = (props) => {
-    return (
-        <li>{props.name}</li>
-    );
+  return (
+    <Box mb="16px">
+      <Checkbox colorScheme="blue" size="lg">
+        <Text>{props.name}</Text>
+      </Checkbox>
+    </Box>
+  );
 };
 
 export default Task;
