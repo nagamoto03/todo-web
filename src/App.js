210783ee4275154efbbfb7ba7823e319d9c2ba5e
nk19963 <kn_1996_0303@yahoo.co.jp>
2024-02-25 11:33:13 +0900

    make Task component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import Task from "./component/Task";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>タスク一覧</h1>
+      <Task name="買い物" />
+      <Task name="ランニング" />
+      <Task name="プログラミングの勉強" />
+    </>
   );
-}
+};
 
 export default App;
